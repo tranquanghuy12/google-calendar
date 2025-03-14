66e9f47d63ae1115035e565dfe01acf694751be4
huytq-fe-dev <tranquanghuy12h@gmail.com>
2025-03-14 17:46:10 +0700

    Fix something

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,8 @@ import Header from "@/components/header/Header";
 import MainView from "@/components/MainView";
 import { db } from "@/db/drizzle";
 import { CalendarEventType, RecurringRuleType } from "@/lib/store";
+import { generateRecurringDates } from "./actions/generate";
 // import dayjs from "dayjs";
-
-import { addDays, addWeeks, addMonths, isBefore } from "date-fns";
-
-export function generateRecurringDates(
-  startDate: Date,
-  rule: RecurringRuleType,
-) {
-  const dates = [];
-  let currentDate = new Date(startDate);
-  const { frequency, interval, count, until } = rule;
-
-  while (
-    (count ? dates.length < count : true) &&
-    (until ? isBefore(currentDate, new Date(until)) : true)
-  ) {
-    dates.push(new Date(currentDate));
-
-    if (frequency === "daily") {
-      currentDate = addDays(currentDate, interval || 1);
-    } else if (frequency === "weekly") {
-      currentDate = addWeeks(currentDate, interval || 1);
-    } else if (frequency === "monthly") {
-      currentDate = addMonths(currentDate, interval || 1);
-    }
-  }
-
-  return dates;
-}
 
 const getEventsData = async () => {
   try {
